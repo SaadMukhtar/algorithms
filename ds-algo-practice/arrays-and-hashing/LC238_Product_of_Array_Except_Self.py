6eaadd4388c13e6d3c32595c8389af6d208d0c48
Saad Mukhtar <saadmukhtar01@gmail.com>
2025-07-31 07:49:46 -0400

    LC238

--- ds-algo-practice/arrays-and-hashing/LC238_Product_of_Array_Except_Self.py
+++ ds-algo-practice/arrays-and-hashing/LC238_Product_of_Array_Except_Self.py
@@ -1,11 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
 
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+        prefixProducts = []
+        suffixProducts = []
+        product = 1
+        for i, n in enumerate(nums):
+            product *= n
+            prefixProducts.append(product)
+        product = 1
+        for i, n in enumerate(nums[::-1]):
+            product *= n
+            suffixProducts.append(product)
+        suffixProducts = suffixProducts[::-1]
+        res = []
+        for i in range(len(nums)):
+            prefixProduct = prefixProducts[i-1] if i >= 1 else 1
+            suffixProduct = suffixProducts[i+1] if i < len(nums)-1 else 1
+            res.append(prefixProduct * suffixProduct)
         return res
