d9538519090ae4b24cdb54b2b0938f3cb4a6e98d
Saad Mukhtar <saadmukhtar01@gmail.com>
2025-07-31 07:09:48 -0400

    LC242

--- ds-algo-practice/arrays-and-hashing/LC242_Valid_Anagram.py
+++ ds-algo-practice/arrays-and-hashing/LC242_Valid_Anagram.py
@@ -1,14 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counterS = Counter(s)
-        counterT = Counter(t)
-        if len(counterS) != len(counterT):
-            return False
-
-        for k, v in counterS.items():
-            if k not in counterT:
-                return False
-            if v != counterT[k]:
-                return False
-        return True
-            
+        sDict = Counter(s)
+        tDict = Counter(t)
+        return sDict == tDict
