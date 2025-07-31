2c09988b27ff17197ce7ffa994e25559e1ecdc0f
Saad Mukhtar <saadmukhtar01@gmail.com>
2025-07-31 07:13:51 -0400

    LC49

--- ds-algo-practice/arrays-and-hashing/LC49_Group_Anagrams.py
+++ ds-algo-practice/arrays-and-hashing/LC49_Group_Anagrams.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anaMap = defaultdict(list)
-        for s in strs:
-            anaMap["".join(sorted(s))].append(s)
-        return list(anaMap.values())
-    
+        anagramMap = defaultdict(list)
+        for w in strs:
+            sortedW = "".join(sorted(w))
+            anagramMap[sortedW].append(w)
+        return list(anagramMap.values())
